bf62fc89b82baadd35fdb1223a4786fd8255242f
Diana Gomez <dianacarolinagomez8@gmail.com>
2025-07-04 09:10:12 -0500

    Proyecto corregido con Tailwind funcionando

---
+++ React-2/postcss.config.cjs
@@ -0,0 +1,8 @@
+// postcss.config.cjs
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+// This configuration file sets up PostCSS to use Tailwind CSS and Autoprefixer.
--- React-2/tailwind.config.js
+++ React-2/tailwind.config.js
@@ -1,9 +1,9 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
